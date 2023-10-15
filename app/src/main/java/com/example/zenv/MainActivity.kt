98881b930495665ed1090ab49d49f19da7188039
iar98 <irvanalrasyid0@gmail.com>
2023-10-15 21:15:16 +0700

    Penambahan feature RecycleView

--- app/src/main/java/com/example/zenv/MainActivity.kt
+++ app/src/main/java/com/example/zenv/MainActivity.kt
@@ -2,19 +2,54 @@ package com.example.zenv
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.zenv.adapters.RvItem
 import com.example.zenv.databinding.ActivityMainBinding
+import com.example.zenv.model.ItemModel
 
 class MainActivity : AppCompatActivity() {
-
+    // declaration binding
     private lateinit var binding: ActivityMainBinding
+    private lateinit var adapterMain: RvItem
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        installSplashScreen()
-        setContentView(R.layout.activity_main)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        init()
+        setRvAdapter()
     }
+
+    // LayoutManager
+    private fun init() {
+        binding.card1.layoutManager = LinearLayoutManager(this@MainActivity)
+    }
+
+    // set array
+    private fun setRvAdapter() {
+        val dataList: MutableList<ItemModel> = mutableListOf()
+
+        nameHead().forEachIndexed{index, name ->
+            dataList.add(ItemModel(ImageItem().get(index),name,descName()[index],dateName()[index],btnName()[index], btnName2()[index]))
+        }
+
+        Log.d("ISI DATANYA", dataList.toString())
+        adapterMain = RvItem(dataList)
+        binding.card1.adapter =adapterMain
+    }
+
+    // array
+    private fun nameHead(): Array<String> = resources.getStringArray(R.array.data_name)
+    private fun descName(): Array<String> = resources.getStringArray(R.array.data_description)
+    private fun dateName(): Array<String> = resources.getStringArray(R.array.data_date)
+    private fun btnName(): Array<String> = resources.getStringArray(R.array.data_btn)
+    private fun btnName2(): Array<String> = resources.getStringArray(R.array.data_btn2)
+    private fun ImageItem():List<Int> = listOf(
+        R.drawable.photo1,
+        R.drawable.photo2,
+        R.drawable.photo3,
+        R.drawable.photo4
+    )
 }
---
+++ app/src/main/java/com/example/zenv/model/ItemModel.kt
@@ -0,0 +1,11 @@
+package com.example.zenv.model
+
+data class ItemModel(
+    // popular data
+    val image: Int,
+    val name: String,
+    val desc: String,
+    val date: String,
+    val btn: String,
+    val btn2: String,
+)
